9d7326cb8e77cb45d748e57adb21d2147ece0289
Balaji <balajiraj92@yahoo.co.in>
2020-08-04 15:47:24 +0530

    RestAssured project

---
+++ Mavetest/src/main/java/com/restassur/base/RestBasic.java
@@ -0,0 +1,66 @@
+package com.restassur.base;
+
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+
+public class RestBasic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		RequestSpecification spec = RestAssured.given();
+//		Response response =	spec.get("https://www.google.com");
+//		response.prettyPrint();
+//		ValidatableResponse vr = response.then();
+//					vr.statusCode(200);
+		RestAssured.given()
+					.expect()
+					.statusCode(200)
+					.when()
+					.get("https://www.google.com");
+		
+		RestAssured.given()
+		.when()
+		.get("https://www.google.com")
+		.then()
+		.statusCode(200);
+		
+		
+					
+		
+
+	}
+	@Test
+	void te() {
+		
+		Response re =
+		RestAssured.given()
+		.queryParam("statuscode", "200")
+		.when()
+		.get("https://www.google.com");
+		
+		re.prettyPrint();
+		
+		
+	}
+	
+	@Test
+	void te1() {
+		
+		Response re =
+		RestAssured.given()
+		.pathParam("id", 2)
+		.when()
+		.get("https://www.google.com");
+		
+		re.prettyPrint();
+		
+		
+	}
+	
+
+}
