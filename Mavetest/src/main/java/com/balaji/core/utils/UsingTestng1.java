29a24291dd257d6102ba7986d6291132269bae38
Balaji <balajiraj92@yahoo.co.in>
2020-08-10 14:37:23 +0530

    Added Data provider script successfully [testng]

---
+++ Mavetest/src/main/java/com/balaji/core/utils/UsingTestng1.java
@@ -0,0 +1,84 @@
+package com.balaji.core.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.util.CellUtil;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class UsingTestng1 {
+	
+	public static Sheet sheet;
+	public static Row row;
+	public static Map<String, String>[][] map = null;
+	
+	@Test(dataProvider= "LoginDataProvider")
+	public void logintest(String email, String pwd) {
+		System.out.println(email+" " +pwd);
+	}
+	
+	@DataProvider(name="LoginDataProvider")
+	public Object[][] getData(){
+		
+		Object[][] data= {{"abc@gmail.com", "abc"}, {"abcd@gmail.com", "nnn"}};
+		return data;
+		
+	}
+	
+	@DataProvider(name="LoginDataProvider")
+	public static Map<String, String>[][] readData() throws IOException{
+		
+		
+		String path="path";
+		File file = new File(path);
+		
+		FileInputStream inputStream = new FileInputStream(file);
+		Workbook workbook = new XSSFWorkbook(inputStream);
+		sheet = workbook.getSheet("");
+		map = new LinkedHashMap<String, String>();
+		int lastRow = sheet.getLastRowNum();
+		row= sheet.getRow(0);
+		List<String> lt = new LinkedList<String>();
+		
+		for(int i=1; i<lastRow+1; i++) {
+			Row r = CellUtil.getRow(i, sheet);
+			String key = CellUtil.getCell(r, 3).getStringCellValue();
+			lt.add(key);
+			lt.removeIf(item -> item == null ||"".equals(item));
+		}
+		
+		for(String str : lt) {
+			for(String s: str.split("\n")) {
+				String[] kv = s.split("=");
+				System.out.println(kv[0].trim()+" , "+kv[1].trim());
+				map.put(kv[0].trim(), kv[1].trim());
+			}
+		}
+		
+		
+		return map;
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
